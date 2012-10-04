1f554c2982a70ce1300b25b4d6c4e1e7a6ceca25
Taewoo Kim <codevania@gmail.com>
2012-10-04 16:54:01 +0900

    3번 워닝 제거
    루비의 모든 변수는 객체기 때문에 발생했던 경고 메세지.
    @를 붙이니 워닝 해결됨. (이게 좀 귀찮;;)

--- Problem 3.rb
+++ Problem 3.rb
@@ -1,24 +1,22 @@
 
-Num = 600851475143
-Factor = 2
-MaxFactor = 1
+@Num = 600851475143
+@Factor = 2
+@MaxFactor = 1
 
-while ( Num > 1 )
+while ( @Num > 1 )
 
-	if ( Num % Factor == 0 )
-		MaxFactor = Factor
-		Num = Num / Factor
+	if ( @Num % @Factor == 0 )
+		@MaxFactor = @Factor
+		@Num = @Num / @Factor
 
-		while ( Num % Factor == 0 )
-			Num = Num / Factor
+		while ( @Num % @Factor == 0 )
+			@Num = @Num / @Factor
 		end
 	end
 
 	# print Num.to_s + " " + Factor.to_s + " " + MaxFactor.to_s + "\n"
 	
-	Factor = Factor + 1
+	@Factor = @Factor + 1
 end
 
-print "\n"
-print MaxFactor
-print "\n"
+print @MaxFactor
